1e92bbd5e6caedd7306fe9bd406fbea6139755f4
mthang24242424 <wunkevan@gmail.com>
2024-11-25 20:38:43 +0700

    ss5ex2

---
+++ ss5ex2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int n = 5;
+	printf("nhap so nguyen ");
+	scanf("%d",&n);
+	while(n!=5){
+	 printf("moi ban nhap lai so nguyen");
+	 scanf("%d",&n);
+	}
+	return 0;
+}
