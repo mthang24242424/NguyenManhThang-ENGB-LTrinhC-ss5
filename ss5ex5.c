e953cd73e463242d14e954f97874f0dddc9496cd
mthang24242424 <wunkevan@gmail.com>
2024-11-25 21:28:41 +0700

    ss5ex5

--- ss5ex5.c
+++ ss5ex5.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
-	int a,j; 
+	int a; 
 	for(a=1;a<10;a++){
+		printf("------------------------------"); 
 		printf("bang cuu chuong %d \n",a);
 		 for(int i=1;i<11;i++){
 		printf("%d nhan %d bang %d \n",a,i,a*i);
